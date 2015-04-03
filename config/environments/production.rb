2848573284f0c51f8fd4821df74c0edfe7ee9394
madebyeggs <madebyeggs@gmail.com>
2015-04-03 01:16:23 +0100

    :s3_protocol => 'https'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,5 +71,6 @@ V1::Application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
+    :s3_protocol => 'https'
   }
 end
