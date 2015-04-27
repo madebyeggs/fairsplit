8deacb235f7e3dde4bebb9b41355e8c4e9beddcd
madebyeggs <madebyeggs@gmail.com>
2015-04-27 14:10:39 +0100

    bug fixing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
     @works = Work.order("id DESC")
     @newest = Work.last
     @artists = Artist.order("latest DESC, name ASC")
-    @artist = Artist.find(1)
     @announcements = Announcement.order("latest DESC, id DESC")
     @sounds = Sound.order("latest DESC")
   end
