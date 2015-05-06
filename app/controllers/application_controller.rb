a121f78aa81629ac5ac20b801078fae6ecb92631
madebyeggs <madebyeggs@gmail.com>
2015-05-06 11:49:41 +0100

    work now ordered by latest, then id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   def bring_in_models
-    @works = Work.order("id DESC")
+    @works = Work.order("latest DESC, id DESC")
     @newest = Work.last
     @artists = Artist.order("latest DESC, name ASC")
+    @artist = Artist.first
     @announcements = Announcement.order("latest DESC, id DESC")
+    @combined_sorted = (Announcement.all + Artist.where("latest = ?", true)).sort{|a,b| a.created_at <=> b.created_at }
     @sounds = Sound.order("latest DESC")
   end
   
