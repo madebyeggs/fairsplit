48335a863b6f404b067aa0e0d91c1b98afb48654
madebyeggs <madebyeggs@gmail.com>
2015-05-07 12:41:10 +0100

    re add square_image to work

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,6 @@
 class Work < ActiveRecord::Base
   attr_accessible :title, :client, :description, :vimeo, :image, :large_image, :type_of_work, :artist_name, :track_name, 
-  :latest, :artist_id, :homepage_title, :uid, :is_artist, :is_work, :is_sound, :is_announcement
+  :latest, :artist_id, :soundcloud, :homepage_title, :square_image, :uid, :is_artist, :is_work, :is_sound, :is_announcement
   
   before_save :create_unique_id
   belongs_to :artist
@@ -17,12 +17,21 @@ class Work < ActiveRecord::Base
       square: '200x200#',
       main: '710x400>'
     }
+    
+    has_attached_file :square_image, styles: {
+      thumb: '100x100>',
+      square: '200x200#',
+      main: '300X300>'
+    }
 
     # Validate the attached image is image/jpg, image/png, etc
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
     
     # Validate the attached image is image/jpg, image/png, etc
     validates_attachment_content_type :large_image, :content_type => /\Aimage\/.*\Z/
+    
+    # Validate the attached image is image/jpg, image/png, etc
+    validates_attachment_content_type :square_image, :content_type => /\Aimage\/.*\Z/
     
     def create_unique_id
       self.uid = rand.to_s[2..16]
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150507113239) do
+ActiveRecord::Schema.define(:version => 20150507111505) do
 
   create_table "announcements", :force => true do |t|
     t.string   "homepage_title"
@@ -103,8 +103,8 @@ ActiveRecord::Schema.define(:version => 20150507113239) do
     t.string   "client"
     t.text     "description"
     t.string   "vimeo"
-    t.datetime "created_at",               :null => false
-    t.datetime "updated_at",               :null => false
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
     t.string   "artist_name"
     t.string   "type_of_work"
     t.string   "image_file_name"
@@ -124,6 +124,11 @@ ActiveRecord::Schema.define(:version => 20150507113239) do
     t.boolean  "is_work"
     t.boolean  "is_sound"
     t.boolean  "is_announcement"
+    t.string   "soundcloud"
+    t.string   "square_image_file_name"
+    t.string   "square_image_content_type"
+    t.integer  "square_image_file_size"
+    t.datetime "square_image_updated_at"
   end
 
 end
