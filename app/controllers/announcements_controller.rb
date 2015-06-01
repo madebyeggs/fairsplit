48dcb492a76bf865edf35806671da68a480c4904
madebyeggs <madebyeggs@gmail.com>
2015-06-01 20:28:01 +0100

    links done

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -51,13 +51,8 @@ class AnnouncementsController < ApplicationController
     def create_links
       @announcement = Artist.find(params[:id])
       bitly = Bitly.new(ENV['BITLY_USER'],ENV['BITLY_PASS'])
-		  id_url = bitly.shorten("https://fairsplitmusic.com/#filter=.announcements/" + "announcement" + "#{@announcement.id}")
 		  uid_url = bitly.shorten("https://fairsplitmusic.com/#filter=.announcements/" + "announcement" + "#{@announcement.uid}")
-		  short_id_url = id_url.short_url
 		  short_uid_url = uid_url.short_url
-      if @announcement.short_id_url == '' || @announcement.short_id_url.blank?
-			  @announcement.short_id_url = short_id_url
-			end
 			if @announcement.short_uid_url == '' || @announcement.short_uid_url.blank?
 			  @announcement.short_uid_url = short_uid_url
 			end
