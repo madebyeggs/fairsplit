c2c9de6694ff3f8d56085e8a36fb1cf1f451caa7
madebyeggs <madebyeggs@gmail.com>
2015-06-02 14:46:47 +0100

    backend URLs working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -128,6 +128,72 @@ $(document).ready(function() {
 		infowindow.open(map,marker);
 
 		/* ========= End Marker ========= */
+		
+		//artist open from URL 
+		var str = window.location.href;
+		if (str.toLowerCase().indexOf("announcements/") >= 0) {
+			var announcement_id = str.split("#")[2];
+			announcement_urlLaunch(announcement_id);
+		}
+		else if (str.toLowerCase().indexOf("works/") >= 0) {
+			var work_id = str.split("#")[2];
+			work_urlLaunch(work_id);
+		}
+		else if (str.toLowerCase().indexOf("artists/") >= 0) {
+			var artist_id = str.split("#")[2];
+			artist_urlLaunch(artist_id);
+		}
+		else if (str.toLowerCase().indexOf("playlists/") >= 0) {
+			var playlist_id = str.split("#")[2];
+			playlist_urlLaunch(playlist_id);
+		}
+		else {
+			clear_all();
+		}
+		function announcement_urlLaunch(announcement_id) {
+			var div_id = '#' + 'announcement' + announcement_id;
+			$(div_id).slideDown(500);
+			$(".element").children(':not(.projectInfo)').fadeTo("fast", 0.3);
+			$(".muteEffects").addClass("displayNone");
+			$(document).ready(function() {
+				$('#container').isotope({ filter: '.announcements' });
+				$('.canScroll5').animate({scrollTop:$('#scrollHereAnnouncement' + announcement_id).position().top + 0}, 'slow');
+			})
+		}
+		function work_urlLaunch(work_id) {
+			var div_id = '#' + 'work' + work_id;
+			$(div_id).slideDown(500);
+			$(".element").children(':not(.projectInfo)').fadeTo("fast", 0.3);
+			$(".muteEffects").addClass("displayNone");
+			$(document).ready(function() {
+				$('#container').isotope({ filter: '.works' });
+				$('.canScroll2').animate({scrollTop:$('#scrollHereWork' + work_id).position().top + 0}, 'slow');
+			})
+		}
+		function artist_urlLaunch(artist_id) {
+			var div_id = '#' + 'artist' + artist_id;
+			$(div_id).slideDown(500);
+			$(".element").children(':not(.projectInfo)').fadeTo("fast", 0.3);
+			$(".muteEffects").addClass("displayNone");
+			$(document).ready(function() {
+				$('#container').isotope({ filter: '.artists' });
+				$('.canScroll4').animate({scrollTop:$('#scrollHereArtist' + artist_id).position().top + 0}, 'slow');
+			})
+		}
+		function playlist_urlLaunch(playlist_id) {
+			var div_id = '#' + 'playlist' + playlist_id;
+			$(div_id).slideDown(500);
+			$(".element").children(':not(.projectInfo)').fadeTo("fast", 0.3);
+			$(".muteEffects").addClass("displayNone");
+			$(document).ready(function() {
+				$('#container').isotope({ filter: '.playlists' });
+				$('.canScroll3').animate({scrollTop:$('#scrollHerePlaylist' + playlist_id).position().top + 0}, 'slow');
+			})
+		}
+		function clear_all() {
+			$(".element").children(':not(.projectInfo)').fadeTo("slow", 1);
+			$(".muteEffects").removeClass("displayNone");
+		}
 	});
 	
 	$('.fitvids').fitVids();
