eb835dfa2cda1315b412095f72e3afd6e161ec29
madebyeggs <madebyeggs@gmail.com>
2015-10-23 16:20:01 +0100

    add flush in works show action to render facebook sdk with turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -65,22 +65,6 @@ function applicash(){
 	
 	$(".tips").tooltip();
 	
-	window.fbAsyncInit = function() {
-		FB.init({
-	    	appId      : '1723612194533881',
-	      	xfbml      : true,
-	      	version    : 'v2.5'
-	    });
-	};
-
-	(function(d, s, id){
-		var js, fjs = d.getElementsByTagName(s)[0];
-	    if (d.getElementById(id)) {return;}
-	    js = d.createElement(s); js.id = id;
-	    js.src = "//connect.facebook.net/en_US/sdk.js";
-	    fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
-	
 	imageHover();
 	
 }
